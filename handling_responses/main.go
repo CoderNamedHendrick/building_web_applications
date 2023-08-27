dae5ceda7e7a55d76866cf4cc3e082fff4f6256f
Sebastine Odeh <sebastinesoacatp@gmail.com>
2023-08-27 09:11:15 +0100

    session cookies

--- handling_responses/main.go
+++ handling_responses/main.go
@@ -1,11 +1,42 @@
 package main
 
 import (
+	"encoding/base64"
 	json2 "encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 )
+
+// transient messaging
+func setMessage(w http.ResponseWriter, r *http.Request) {
+	msg := []byte("Hello World!")
+	c := http.Cookie{
+		Name:  "flash",
+		Value: base64.URLEncoding.EncodeToString(msg),
+	}
+	http.SetCookie(w, &c)
+}
+
+func showMessage(w http.ResponseWriter, r *http.Request) {
+	c, err := r.Cookie("flash")
+	if err != nil {
+		if errors.Is(err, http.ErrNoCookie) {
+			fmt.Fprintln(w, "No message found")
+		}
+	} else {
+		rc := http.Cookie{
+			Name:    "flash",
+			MaxAge:  -1,
+			Expires: time.Unix(1, 0),
+		}
+		http.SetCookie(w, &rc)
+		val, _ := base64.URLEncoding.DecodeString(c.Value)
+		fmt.Fprintln(w, string(val))
+	}
+}
 
 func main() {
 
@@ -19,7 +50,37 @@ func main() {
 	http.HandleFunc("/writeheader", writeHeaderExample)
 	http.HandleFunc("/redirect", headerExample)
 	http.HandleFunc("/json", jsonExample)
+	http.HandleFunc("/set_cookie", setCookie)
+	http.HandleFunc("/get_cookie", getCookie)
+	http.HandleFunc("/set_message", setMessage)
+	http.HandleFunc("/show_message", showMessage)
 	server.ListenAndServe()
+}
+
+func setCookie(w http.ResponseWriter, r *http.Request) {
+	c1 := http.Cookie{
+		Name:     "first_cookie",
+		Value:    "Go Web Programming",
+		HttpOnly: true,
+	}
+	c2 := http.Cookie{
+		Name:     "second_cookie",
+		Value:    "Manning Publications Co",
+		HttpOnly: true,
+	}
+
+	http.SetCookie(w, &c1)
+	http.SetCookie(w, &c2)
+}
+
+func getCookie(w http.ResponseWriter, r *http.Request) {
+	c1, err := r.Cookie("first_cookie")
+	if err != nil {
+		fmt.Fprintln(w, "Cannot get the first cookie")
+	}
+	cs := r.Cookies()
+	fmt.Fprintln(w, c1)
+	fmt.Fprintln(w, cs)
 }
 
 type Post struct {
