5bfd36eeae512e5528b4f0e3469a5f321fdd83ae
Sebastine Odeh <sebastinesoacatp@gmail.com>
2023-08-27 08:48:50 +0100

    working with responses

--- handling_responses/main.go
+++ handling_responses/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	json2 "encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -14,7 +15,48 @@ func main() {
 	http.HandleFunc("/headers", headers)
 	http.HandleFunc("/body", body)
 	http.HandleFunc("/process", process)
+	http.HandleFunc("/write", writeExample)
+	http.HandleFunc("/writeheader", writeHeaderExample)
+	http.HandleFunc("/redirect", headerExample)
+	http.HandleFunc("/json", jsonExample)
 	server.ListenAndServe()
+}
+
+type Post struct {
+	User    string
+	Threads []string
+}
+
+// sending data in response body
+func writeExample(w http.ResponseWriter, r *http.Request) {
+	str := `<html>
+<head><title>Go Web Programming</title></head>
+<body><h1>Hello World</h1></body>
+</html>`
+	w.Write([]byte(str))
+}
+
+// sending response with status code
+func writeHeaderExample(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(501)
+	fmt.Fprintln(w, "No such service, try next door")
+}
+
+// sending response tweaking the headers
+func headerExample(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Location", "http://google.com")
+	w.WriteHeader(302)
+}
+
+// returning json response
+func jsonExample(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	post := &Post{
+		User:    "Sebastine",
+		Threads: []string{"first", "second", "third"},
+	}
+	json, _ := json2.Marshal(post)
+	w.Write(json)
 }
 
 func headers(w http.ResponseWriter, r *http.Request) {
